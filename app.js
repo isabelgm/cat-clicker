28a9b75bed20955c7da9df2aa392b50387b482c7
isa <isabel@juntostudio.com>
2017-06-12 14:07:23 -0400

    Add 5 cats you can choose from

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
-var counter1 = 0;
-$('#cat1').click(function() {
-  counter1++;
-  console.log(counter1);
-  $('#counter1').html("Clicks: " + counter1);
-  return false;
+var counter = 0;
+
+$('#cat-1').click(function() {
+  counter = 0;
+  $('#counter').html("Clicks: " + counter);
+  $('#cat-name').html("Kitty Eyes");
+  $('#cat-img').attr("src", "images/cat1.jpg");
 });
 
-var counter2 = 0;
-$('#cat2').click(function() {
-  counter2++;
-  console.log(counter2);
-  $('#counter2').html("Clicks: " + counter2);
-  return false;
+$('#cat-2').click(function() {
+  counter = 0;
+  $('#counter').html("Clicks: " + counter);
+  $('#cat-name').html("Blue-Eyed Cat");
+  $('#cat-img').attr("src", "images/cat2.jpg");
+});
+
+$('#cat-3').click(function() {
+  counter = 0;
+  $('#counter').html("Clicks: " + counter);
+  $('#cat-name').html("Snuggle Buddies");
+  $('#cat-img').attr("src", "images/cat3.jpg");
+});
+
+$('#cat-4').click(function() {
+  counter = 0;
+  $('#counter').html("Clicks: " + counter);
+  $('#cat-name').html("Siesta Time");
+  $('#cat-img').attr("src", "images/cat4.jpg");
+});
+
+$('#cat-5').click(function() {
+  counter = 0;
+  $('#counter').html("Clicks: " + counter);
+  $('#cat-name').html("Park Friends");
+  $('#cat-img').attr("src", "images/cat5.jpg");
+});
+
+$('#cat-img').click(function(catCopy){
+  counter++;
+  console.log(counter);
+  $('#counter').html("Clicks: " + counter);
 });
